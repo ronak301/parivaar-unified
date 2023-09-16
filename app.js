eca477a85622ea1008545283a08c06713e03c1fd
st1llwater <alokpunjofficial@gmail.com>
2023-09-16 23:42:57 +0530

    feat: port changes

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.use('/relationship', relationshipRoutes);
 app.use('/executive', executiveRoutes);
 app.use('/address', addressRoutes);
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(80, () => {
+  console.log('Server started on port 80');
 });
